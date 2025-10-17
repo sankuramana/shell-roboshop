c530c9b542980b656a1940f20f1ffdd035fba549
ramana sanku <sankuramana456@gmail.com>
2025-10-17 10:21:56 +0530

    this is roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -24,7 +24,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
         echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo 
 VALIDATE $? "adding/copying mongorepo"
  
 dnf install mongodb-org -y & >>$LOG_FILE
