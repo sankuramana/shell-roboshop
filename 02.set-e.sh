71516e80181b82c1a574e63c992e8f6638d2a434
ramana sanku <sankuramana456@gmail.com>
2025-10-18 10:49:42 +0530

    this is roboshop

--- 02.set-e.sh
+++ 02.set-e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-trap 'echo "there is an error in $LINENO, command is : $BASH_COMMAND"' 
+trap 'echo "there is an error in $LINENO, command is : $BASH_COMMAND"' ERR
 echo "Hello.."
 echo "before error"
 embavc
