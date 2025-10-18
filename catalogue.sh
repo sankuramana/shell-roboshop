f5c1a51e5472e2e1de24f02b61c009dcff442a56
ramana sanku <sankuramana456@gmail.com>
2025-10-18 08:46:32 +0530

    this is roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-MONGODB_HOST=mongodb.aws45.fun
+MONGODB_HOST=mongodb.aws45.fun 
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
