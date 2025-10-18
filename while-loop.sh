e47c42334cfff8beb881a19c2f69a73435393adf
ramana sanku <sankuramana456@gmail.com>
2025-10-18 21:35:29 +0530

    this is roboshop

--- while-loop.sh
+++ while-loop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 count=$1
-echo "count down....."
-while [ $count -ne 25 ]
+echo "count numbers increment....."
+while [ $count -ne 25 ] #[ $count -gt 0  ]
 do
 echo " time left ::..$count"
 #count=$((count -1)) #decrement the count
-count=$((count+1))
+count=$((count+1)) #increment the count
 done
 echo "time is up"
